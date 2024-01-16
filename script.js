a56101a23677ed692a8222e074d7430795ea9f0a
Alka Rani <143867644+alka9689@users.noreply.github.com>
2024-01-16 12:53:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let submit=document.querySelector(".submit");
+submit.onclick=()=>{
+    window.alert("Data is Submitted");
+}
+let ticket=document.querySelector(".ticket");
+ticket.onclick=()=>{
+    window.alert("Loading...");
+}
